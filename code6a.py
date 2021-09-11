de89607c29bef163900fc921236e231469da3900
Khoa Nguyen <mkhoa0201@gmail.com>
2021-09-11 10:10:26 +0300

    Python task 1

---
+++ code6a.py
@@ -0,0 +1,33 @@
+#1. Return avg of 2 interger
+
+def average(a,b):
+    avg = (a+b)/2
+    return avg
+
+result = average(10,30)
+print(result)
+
+#2. Return avg of 4 floating point
+
+def average2(c,d,e,f):
+    avg2 = (e+f+c+d)/4
+
+    return avg2
+
+res = average2(1.2,2.3,3.4,4.5)
+print(res)
+
+#3. Returns the sum of an array.
+
+g = range(10)
+print(g)
+h = sum(g)
+print(h)
+
+
+
+
+
+
+
+
