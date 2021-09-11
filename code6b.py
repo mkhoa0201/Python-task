2d071b6f1855781b8768ef2f7ce41d61f6331310
Khoa Nguyen <mkhoa0201@gmail.com>
2021-09-11 10:14:15 +0300

    Python task 2

---
+++ code6b.py
@@ -0,0 +1,10 @@
+#4. Returns the factorial.
+
+n = input("Enter a number: ")
+factorial = 1
+if int(n) >= 1:
+    for i in range (1,int(n)+1):
+        factorial = factorial * i
+
+        
+print("Factorail of ",n , " is : ",factorial)
